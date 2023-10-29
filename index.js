7c3254e3372d46521368d3eb3a68d35dbcbc8b3f
Steban01 <esteban.cortes@correounivalle.edu.co>
2023-10-29 09:50:14 -0600

    configuracion json index2

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-import { ApolloServer, gql } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import { buildSubgraphSchema } from "@apollo/subgraph";
-import http from "http";
-import express from "express";
-import cors from "cors";
-import { typeDefs, resolvers } from "./src/configs/gateway.js";
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
 
+const express= require('express');
 const app = express();
+const port= process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-
-const httpServer = http.createServer(app);
-
-const startApolloServer = async (app, httpServer) => {
-	const server = new ApolloServer({
-		schema: buildSubgraphSchema({ typeDefs, resolvers }),
-		introspection: true,
-		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-	});
-
-	await server.start();
-	server.applyMiddleware({ app });
-};
-
-startApolloServer(app, httpServer);
-
-app.listen(9000, () =>
-	console.log("🚀 AUTHENTICATION Server ready at http://localhost:9000/graphql")
+app.listen(3000, () =>
+	console.log("🚀 AUTHENTICATION Server ready at http://localhost:3000/graphql")
 );
