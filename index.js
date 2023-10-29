f885d039ca5dc9c798a391b7abbb5b816b8b6734
Steban01 <esteban.cortes@correounivalle.edu.co>
2023-10-28 22:26:38 -0600

    configuracion jsonv6

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const exp = require('constants');
 
+const express= require('express');
 const app = express();
-const PORT = 5000;
+const port= process.env.PORT || 3000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+app.listen(port);
 
-async function startApolloServer() {
-  await server.start();
-  server.applyMiddleware({ app, path: '/graphql' });
-}
-
-startApolloServer().then(() => {
-  app.listen(5000, () => {
-    console.log(`🚀 AUTHENTICATION Server ready at http://localhost:5000/graphql`);
-  });
-});
+console.log('API gateway escuchando en el puerto: ' + port);
