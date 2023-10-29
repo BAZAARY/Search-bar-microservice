b198aff55bd616199e112ae1902da9ac9c18785b
Steban01 <esteban.cortes@correounivalle.edu.co>
2023-10-29 10:13:30 -0600

    configuracion json index3

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 
-const express= require('express');
+
 const app = express();
 const port= process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(3000, () =>
-	console.log("🚀 AUTHENTICATION Server ready at http://localhost:3000/graphql")
+	console.log("🚀 AUTHENTICATION Server ready at http://localhost:3000")
 );
+
+
