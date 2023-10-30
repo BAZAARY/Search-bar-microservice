9cd0088bfe71941e6c8615cc2fdef5115c430124
Steban01 <esteban.cortes@correounivalle.edu.co>
2023-10-29 18:56:16 -0600

    creacion de resolvers y scheme segun la funcionalida4

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,36 +1,41 @@
 const fetch = require('node-fetch');
 
-async function callInventoryService(query) {
+async function getProductIDsByQuery(query) {
+  const inventoryURL = 'https://catalogmicroservicegraphql.onrender.com/graphql';
+
   try {
-    const inventoryResponse = await fetch('https://catalogmicroservicegraphql.onrender.com/graphql', {
+    const inventoryResponse = await fetch(inventoryURL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         query: `
-          query getProductNameAndPrice($productName: String!) {
-            getProductNameAndPriceWithId(productId: $productName) {
+          query GetProductIDs($productName: String!) {
+            search(query: $productName) {
               id
-              name
-              price
             }
           }
         `,
         variables: {
-          productName: query // 'query' contiene el nombre del producto a buscar
+          productName: query
         }
       })
     });
 
-    if (inventoryResponse.ok) {
-      const data = await inventoryResponse.json();
-      return data.data.getProductNameAndPriceWithId; // Retorna la información obtenida del inventario
-    } else {
+    if (!inventoryResponse.ok) {
       throw new Error('Error al llamar al servicio de inventario');
     }
+
+    const data = await inventoryResponse.json();
+
+    if (data.errors) {
+      throw new Error('Error en la respuesta del servicio de inventario');
+    }
+
+    return data.data.search.map(product => product.id);
   } catch (error) {
-    throw new Error('Error al comunicarse con el servicio de inventario');
+    throw new Error('Error al comunicarse con el servicio de inventario: ' + error.message);
   }
 }
 
@@ -38,8 +43,8 @@ const resolvers = {
   Query: {
     search: async (_, { query }) => {
       try {
-        const products = await callInventoryService(query);
-        return products;
+        const productIDs = await getProductIDsByQuery(query);
+        return productIDs;
       } catch (error) {
         throw new Error(error.message);
       }
